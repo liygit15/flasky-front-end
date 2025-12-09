042e863f3a42a226cda837063f75460027d4cd8a
liyan <liyangeneral@gmail.com>
2025-12-09 10:35:16 -0800

    finish live code

---
+++ src/components/Cat.jsx
@@ -0,0 +1,11 @@
+const Cat = () => {
+  return (
+    <li>
+      <h2>Name of Cat</h2>
+      <p>Meow!</p>
+      <button>Pet</button>
+    </li>
+  );
+};
+
+export default Cat;
---
+++ src/components/CatList.jsx
@@ -0,0 +1,16 @@
+import Cat from './Cat.jsx';
+const CatList = () => {
+  return (
+    <> 
+      <h1>Cat List</h1>
+      <ul>
+        <Cat />
+        <Cat />
+        <Cat />
+        <Cat />
+      </ul>
+    </>
+  );
+};
+
+export default CatList;
