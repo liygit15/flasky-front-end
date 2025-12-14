57bbbe2440865f1135f45b0dc4d49bbc196d3c6e
liyan <liyangeneral@gmail.com>
2025-12-13 22:11:43 -0800

    add state to update ui

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Cat = ({name, personality, color, caretaker}) => {
+  const [petCount, setPetCount] = useState(0);
+
+  const increasePets = () => {
+    setPetCount((prePetCount) => prePetCount + 1 );
+  };
   return (
     <li className="cat">
       <h2>{name}</h2>
       <p>Color: {color}</p>
       <p>Personality {personality}</p>
       <p>Caretaker {caretaker}</p>
-      <button>Pet</button>
+      <p>Neow! I have been pet {petCount} times {caretaker}</p>
+      <button onClick={event => increasePets()}>Pet</button>
     </li>
   );
 };
