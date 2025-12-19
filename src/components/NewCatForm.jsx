82a4686e7d422849c24ccb2d7e3bdf46b5a05238
liyan <liyangeneral@gmail.com>
2025-12-18 20:28:46 -0800

    wave 5

--- src/components/NewCatForm.jsx
+++ src/components/NewCatForm.jsx
@@ -1,40 +1,39 @@
-// import axios from 'axios';
 import { useState } from 'react';
-
+import PropTypes from 'prop-types';
 
 const NewCatForm = ({onHandleSubmit}) => {
-    const [name, setName] =useState('');
+    const [name, setName] = useState('');
 
     const handleNameChange = (event) => {
-        // console.log(event.target.value);
         setName(event.target.value);
     };
 
-    const handleSubmit = (event) => {
-
+    const handleSubmit = (event) =>
+    {
         event.preventDefault();
-
         const newCat = {
             name,
-            personality:'festive',
-            color:'silver and Gold',
-            // petCount:0
+            personality: 'festive',
+            color: 'Silver and Gold',
+            petCount: 0
         };
         onHandleSubmit(newCat);
         setName('');
     };
-
 
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="name">Cat Name:</label>
             <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/>
             <div>
-                <input type="submit"  value="Add a cat"/>
-                <p value='what'>hello</p>
+                <input type="submit" value="Add a cat" />
             </div>
         </form>
     );
+};
+
+NewCatForm.propTypes = {
+    onHandleSubmit: PropTypes.func.isRequired,
 };
 
 export default NewCatForm;
